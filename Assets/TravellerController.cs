1282958147c014a5690eb93faf5a8bbd26f45724
Quinten Lansu <quintenlansu@gmail.com>
2017-10-28 13:40:38 +0200

    Move character around by clicking on map.

---
+++ Assets/TravellerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TravellerController : MonoBehaviour
+{
+	public int movementSpeed = 3;
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			transform.position = ray.GetPoint(0.0f);
+		}
+	}
+}
