98ffdd0e15639ac92a5675f2da0d007008926a8c
FightingForFun <g.fankhaenel@gmail.com>
2017-11-04 23:31:47 +0100

    Item System (utter confusion about size of stuff)

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Inventory : MonoBehaviour
+{
+    public Image[] itemImages = new Image[numItemSlots];
+    public Item[] items = new Item[numItemSlots];
+
+    public const int numItemSlots = 3;
+
+    public int itemCount = 0;
+
+
+    public void AddItem(Item itemToAdd, int ItemCount)
+    {
+        //look through all items slots to find one that is empty --> then populate it with item
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i] == null)
+            {
+                items[i] = itemToAdd;
+                itemImages[i].sprite = itemToAdd.sprite;
+                itemImages[i].enabled = true;
+                return;
+            }
+        }
+    }
+
+    public void RemoveItem(Item itemToRemove, int amountToRemove)
+    {
+        //look through all items until we finde the desired one that we want to remove
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i] == itemToRemove)
+            {
+                items[i] = null;
+                itemImages[i].sprite = null;
+                itemImages[i].enabled = false;
+                return;
+            }
+        }
+    }
+}
